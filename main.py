0104c2117d7b7cbc1723077d2912b0635e43b350
jdayyxx <119957130+jdayyxx@users.noreply.github.com>
2024-12-10 13:12:42 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from myserver import server_on
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ server_on
+
+ bot.run(os.getenv('TOKEN'))
